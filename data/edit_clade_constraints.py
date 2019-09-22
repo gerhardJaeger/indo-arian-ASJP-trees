1f7f70c577df63d31e369a4d300562aa891c3200
Chundra Cathcart <chundra.a.cathcart@gmail.com>
2019-09-22 17:04:50 +0200

    added more node calibrations

--- data/edit_clade_constraints.py
+++ data/edit_clade_constraints.py
@@ -71,3 +71,42 @@ for n in ctree.get_descendants():
 
 
 ctree.write(format=9,outfile='constraints2.tre')
+
+
+clade_calib_anc = {
+    'PIA':('sans1269','Unif(3400,3000)'),
+    'OP':('oldp1254','Unif(2500,2300)'),
+    'MP':('pahl1241','Unif(1800,1400)')
+}
+
+
+clade_calib = {
+    'PIIr':'Unif(3900,3600)',
+    'PIr':'Unif(3500,3300)'
+}
+
+
+f = open('node_calibration.txt','w')
+
+print("""#each block gives (1) the clade name
+#(2) tip which should have a very short branch, if relevant
+#(3) the distribution over the age of the clade
+#(4) the tips in the clade
+""",file=f)
+
+for k in clade_calib_anc.keys():
+    print(k,file=f)
+    print(clade_calib_anc[k][0],file=f)
+    print(clade_calib_anc[k][1],file=f)
+    print(' '.join(ctree.search_nodes(name=k)[0].get_leaf_names()),file=f)
+    print('\n',file=f)
+
+
+for k in clade_calib.keys():
+    print(k,file=f)
+    print(clade_calib[k],file=f)
+    print(' '.join(ctree.search_nodes(name=k)[0].get_leaf_names()),file=f)
+    print('\n',file=f)
+
+
+f.close()
