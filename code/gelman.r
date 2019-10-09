75975fb9c02d7a1d5a965b5e40b2f0cc0acf3ea7
Gerhard Jäger <gerhard.jaeger@uni-tuebingen.de>
2019-10-09 09:18:41 +0200

    convergence diagnostics

---
+++ code/gelman.r
@@ -0,0 +1,31 @@
+library(coda)
+
+posterior1 <- read.table("output/indoiranian_run_1.log",
+                         header=T,check.names=F)[,-1]
+posterior2 <- read.table("output/indoiranian_run_2.log",
+                         header=T,check.names=F)[,-1]
+posterior3 <- read.table("output/indoiranian_run_3.log",
+                         header=T,check.names=F)[,-1]
+posterior4 <-read.table("output/indoiranian_run_4.log",
+                        header=T,check.names=F)[,-1]
+
+n <- min(nrow(posterior1),
+         nrow(posterior2),
+         nrow(posterior3),
+         nrow(posterior4))
+
+cn <- colnames(posterior1)
+
+vcn <- cn[apply(rbind(posterior1[-(1:(n %/% 2)),],
+                      posterior2[-(1:(n %/% 2)),],
+                      posterior3[-(1:(n %/% 2)),],
+                      posterior4[-(1:(n %/% 2)),]),
+                2, var) > 0]
+
+
+chains <- mcmc.list(list(mcmc(data=posterior1[1:n,vcn]),
+                         mcmc(data=posterior2[1:n,vcn]),
+                         mcmc(data=posterior3[1:n,vcn]),
+                         mcmc(data=posterior4[1:n,vcn])))
+
+gelman.diag(chains, multivariate = F)
