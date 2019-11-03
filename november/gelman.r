09ee071c18292360bf2803f76ee6796caf6a9a7b
Gerhard Jäger <gerhard.jaeger@uni-tuebingen.de>
2019-11-03 07:30:57 +0100

    applying to completed chain

--- november/gelman.r
+++ november/gelman.r
@@ -2,14 +2,22 @@ library(coda)
 
 modelName <- "gamma11"
 
-posterior1 <- read.table(paste0("output/", modelName, "_run_1.p"),
-                         header=T,check.names=F)
-posterior2 <- read.table(paste0("output/", modelName, "_run_2.p"),
-                         header=T,check.names=F)
-posterior3 <- read.table(paste0("output/", modelName, "_run_3.p"),
-                         header=T,check.names=F)
-posterior4 <- read.table(paste0("output/", modelName, "_run_4.p"),
-                         header=T,check.names=F)
+## posterior1 <- read.table(paste0("output/", modelName, "_run_1.p"),
+##                          header=T,check.names=F)
+## posterior2 <- read.table(paste0("output/", modelName, "_run_2.p"),
+##                          header=T,check.names=F)
+## posterior3 <- read.table(paste0("output/", modelName, "_run_3.p"),
+##                          header=T,check.names=F)
+## posterior4 <- read.table(paste0("output/", modelName, "_run_4.p"),
+##                          header=T,check.names=F)
+
+posterior <- read.table(paste0("output/", modelName, ".p"),
+                        header=T, check.names=F)
+
+posterior1 <- posterior[posterior$Replicate_ID==0, -2]
+posterior2 <- posterior[posterior$Replicate_ID==1, -2]
+posterior3 <- posterior[posterior$Replicate_ID==2, -2]
+posterior4 <- posterior[posterior$Replicate_ID==3, -2]
 
 
 n <- min(nrow(posterior1),
